6cf45bafdbb8c9d3ea2ed4cf9d45f2589605b32e
antialiasis <antialiasis@gmail.com>
2020-11-02 20:52:47 +0000

    Add Crown Tundra Pokémon.

--- scripts/add-swsh-pokemon.py
+++ scripts/add-swsh-pokemon.py
@@ -16,12 +16,14 @@ PSEUDOFORMS = [
     421,
     422,
     423,
+    649,
+    716,
     773,
     854,
     855,
 ]
 
-ARMOR_DEX_REGEX = re.compile(r'^(\d+) - \[(\d+)\]')
+DEX_REGEX = re.compile(r'^(\d+) - \[(\d+)\]')
 
 def make_identifier(name):
     """Make a string safe to use as an identifier.
@@ -40,9 +42,11 @@ def make_identifier(name):
     identifier = re.sub(u'[ _–]+', u'-', identifier)
     identifier = re.sub(u"['./;’(),:]", u'', identifier)
     identifier = identifier.replace(u'é', u'e')
+    identifier = identifier.replace(u'\u2640', '-f')
+    identifier = identifier.replace(u'\u2642', '-m')
 
     if identifier.startswith('route-'):
-        identifier = 'kalos-' + identifier
+        identifier = 'galar-' + identifier
 
     if not identifier.replace(u"-", u"").isalnum():
         raise ValueError(identifier)
@@ -51,26 +55,35 @@ def make_identifier(name):
 data = json.load(open('C:\\Users\\antialiasis\\Documents\\TCoD\\Flask\\tcod\\tcod\\data-files\\pokemon.json'))
 forms_data = json.load(open(r'C:\\Users\\antialiasis\\Documents\\TCoD\\Flask\\tcod\\scripts\\data-files\\forms.json'))
 armor_dex_file = open('C:\\Users\\antialiasis\\Documents\\TCoD\\Flask\\tcod\\tcod\\data-files\\armor-dex.txt')
+tundra_dex_file = open('C:\\Users\\antialiasis\\Documents\\TCoD\\Flask\\tcod\\tcod\\data-files\\tundra-dex.txt')
 
 armor_dex = {}
 for line in armor_dex_file:
-    match = ARMOR_DEX_REGEX.match(line)
+    match = DEX_REGEX.match(line)
     if not match:
         raise ValueError("Malformatted armor dex line!", line)
     armor_dex[int(match.group(2))] = int(match.group(1))
 
+tundra_dex = {}
+for line in tundra_dex_file:
+    match = DEX_REGEX.match(line)
+    if not match:
+        raise ValueError("Malformatted armor dex line!", line)
+    tundra_dex[int(match.group(2))] = int(match.group(1))
+
 region = "Galar"
 region_ident = make_identifier(region)
-first_form_id = 896
+first_form_id = 901
 
 print("BEGIN;")
-print("INSERT INTO regions (id, identifier) VALUES (8, '%s');" % region_ident)
-print("INSERT INTO region_names (region_id, local_language_id, name) SELECT id, 9, '%s' FROM regions WHERE identifier = '%s';" % (region, region_ident))
-print("INSERT INTO pokedexes (id, region_id, identifier, is_main_series) SELECT 26, id, '%s', true FROM regions WHERE identifier = '%s';" % (region_ident, region_ident))
-print("INSERT INTO pokedexes (id, region_id, identifier, is_main_series) SELECT 27, id, '%s', true FROM regions WHERE identifier = '%s';" % ('isle-of-armor', region_ident))
-print("INSERT INTO generations (id, main_region_id, identifier) SELECT 8, id, '%s' FROM regions WHERE identifier = '%s';" % ('generation-viii', region_ident))
-print("INSERT INTO version_groups (id, identifier, generation_id, \"order\") VALUES (19, 'lets-go', 7, 19);")
-print("INSERT INTO version_groups (id, identifier, generation_id, \"order\") VALUES (20, 'sword-shield', 8, 20);")
+print("INSERT INTO regions (id, identifier) VALUES (8, '%s') ON CONFLICT DO NOTHING;" % region_ident)
+print("INSERT INTO region_names (region_id, local_language_id, name) SELECT id, 9, '%s' FROM regions WHERE identifier = '%s' ON CONFLICT DO NOTHING;" % (region, region_ident))
+print("INSERT INTO pokedexes (id, region_id, identifier, is_main_series) SELECT 26, id, '%s', true FROM regions WHERE identifier = '%s' ON CONFLICT DO NOTHING;" % (region_ident, region_ident))
+print("INSERT INTO pokedexes (id, region_id, identifier, is_main_series) SELECT 27, id, '%s', true FROM regions WHERE identifier = '%s' ON CONFLICT DO NOTHING;" % ('isle-of-armor', region_ident))
+print("INSERT INTO pokedexes (id, region_id, identifier, is_main_series) SELECT 28, id, '%s', true FROM regions WHERE identifier = '%s' ON CONFLICT DO NOTHING;" % ('crown-tundra', region_ident))
+print("INSERT INTO generations (id, main_region_id, identifier) SELECT 8, id, '%s' FROM regions WHERE identifier = '%s' ON CONFLICT DO NOTHING;" % ('generation-viii', region_ident))
+print("INSERT INTO version_groups (id, identifier, generation_id, \"order\") VALUES (19, 'lets-go', 7, 19) ON CONFLICT DO NOTHING;")
+print("INSERT INTO version_groups (id, identifier, generation_id, \"order\") VALUES (20, 'sword-shield', 8, 20) ON CONFLICT DO NOTHING;")
 
 evolution_map = {}
 name_map = {}
@@ -102,9 +115,9 @@ for pokemon in data:
         pokemon_ident = make_identifier(pokemon['name'])
         pokemon_form_ident = pokemon_ident
         species = pokemon
-        if pokemon['name'] not in evolution_map and pokemon['id'] > 807:
+        if pokemon['name'] not in evolution_map and pokemon['id'] > 893:
             print("INSERT INTO evolution_chains (id) SELECT MAX(id) + 1 FROM evolution_chains;")
-        print("INSERT INTO pokemon_species (id, identifier, generation_id, evolves_from_species_id, evolution_chain_id, color_id, shape_id, habitat_id, gender_rate, capture_rate, base_happiness, is_baby, hatch_counter, has_gender_differences, growth_rate_id, forms_switchable, \"order\") SELECT %s, '%s', %s, %s, %s, %s, 1, NULL, -1, 45, 70, false, %s, false, %s, false, %s ON CONFLICT DO NOTHING;" % (
+        print("INSERT INTO pokemon_species (id, identifier, generation_id, evolves_from_species_id, evolution_chain_id, color_id, shape_id, habitat_id, gender_rate, capture_rate, base_happiness, is_baby, hatch_counter, has_gender_differences, growth_rate_id, forms_switchable, is_legendary, is_mythical, \"order\") SELECT %s, '%s', %s, %s, %s, %s, 1, NULL, -1, 45, 70, false, %s, false, %s, false, %s, false, %s ON CONFLICT DO NOTHING;" % (
             pokemon['id'],
             pokemon_ident,
             7 if pokemon_ident in ('meltan', 'melmetal') else 8,
@@ -113,6 +126,7 @@ for pokemon in data:
             '(SELECT id FROM pokemon_colors WHERE identifier = \'%s\')' % pokemon['color'].lower(),
             pokemon['hatch_cycles'],
             '(SELECT id FROM growth_rates WHERE identifier = \'%s\')' % GROWTH_RATES[pokemon['exp_group']],
+            "true" if pokemon_ident in ('regieleki', 'regidrago', 'glastrier', 'spectrier', 'calyrex') else "false",
             pokemon['id']
         ))
 
@@ -122,10 +136,12 @@ for pokemon in data:
         ))
 
         print("INSERT INTO pokemon_dex_numbers (species_id, pokedex_id, pokedex_number) VALUES (%s, 1, %s) ON CONFLICT DO NOTHING;" % (pokemon['id'], pokemon['id']))
-        if pokemon['galar_dex'] != "foreign":
-            print("INSERT INTO pokemon_dex_numbers (species_id, pokedex_id, pokedex_number) VALUES (%s, 26, %s);" % (pokemon['id'], pokemon['galar_dex']))
+        if pokemon['galar_dex'] and pokemon['galar_dex'] != "foreign":
+            print("INSERT INTO pokemon_dex_numbers (species_id, pokedex_id, pokedex_number) VALUES (%s, 26, %s) ON CONFLICT DO NOTHING;" % (pokemon['id'], pokemon['galar_dex']))
         if pokemon['id'] in armor_dex:
-            print("INSERT INTO pokemon_dex_numbers (species_id, pokedex_id, pokedex_number) VALUES (%s, 27, %s);" % (pokemon['id'], armor_dex[pokemon['id']]))
+            print("INSERT INTO pokemon_dex_numbers (species_id, pokedex_id, pokedex_number) VALUES (%s, 27, %s) ON CONFLICT DO NOTHING;" % (pokemon['id'], armor_dex[pokemon['id']]))
+        if pokemon['id'] in tundra_dex:
+            print("INSERT INTO pokemon_dex_numbers (species_id, pokedex_id, pokedex_number) VALUES (%s, 28, %s) ON CONFLICT DO NOTHING;" % (pokemon['id'], tundra_dex[pokemon['id']]))
 
     else:
         name, form_num = pokemon['name'].rsplit(' ', 1)
@@ -144,9 +160,14 @@ for pokemon in data:
                 form_ident = form_ident[:-5]
             elif form_ident == 'original-color':
                 form_ident = 'original'
+            elif form_ident == '50-percent':
+                form_ident = '50'
+            elif form_ident == '10-percent':
+                form_ident = '10'
             pokemon_form_ident += '-' + form_ident
 
     pokemon_order += 1
+    # print("SELECT '%s', '%s', '%s';" % (pokemon_ident, pokemon['id'], species['id']))
     print("INSERT INTO pokemon (id, identifier, species_id, height, weight, base_experience, \"order\", is_default) VALUES (%s, '%s', %s, %s, %s, 65, %s, %s) ON CONFLICT DO NOTHING;" % (
         pokemon['id'] if pokemon['id'] < first_form_id else ("COALESCE((SELECT id FROM pokemon WHERE identifier = '%s'), (SELECT MAX(id) + 1 FROM pokemon))" % (pokemon_ident if species['id'] in PSEUDOFORMS else pokemon_form_ident)),
         pokemon_ident if species['id'] in PSEUDOFORMS else pokemon_form_ident,
@@ -164,21 +185,21 @@ for pokemon in data:
             make_identifier(t)
         ))
 
-    print("INSERT INTO pokemon_forms (id, identifier, form_identifier, pokemon_id, introduced_in_version_group_id, is_default, is_battle_only, is_mega, form_order, \"order\") VALUES (%s, '%s', %s, %s, %s, %s, false, false, 1, (SELECT MAX(\"order\") + 1 FROM pokemon_forms)) ON CONFLICT DO NOTHING;" % (
+    print("INSERT INTO pokemon_forms (id, identifier, form_identifier, pokemon_id, introduced_in_version_group_id, is_default, is_battle_only, is_mega, form_order, \"order\") VALUES (%s, '%s', %s, %s, %s, true, false, false, 1, (SELECT MAX(\"order\") + 1 FROM pokemon_forms)) ON CONFLICT DO NOTHING;" % (
         pokemon['id'] if pokemon['id'] < first_form_id else ("(COALESCE((SELECT id FROM pokemon_forms WHERE identifier = '%s'), (SELECT MAX(id) + 1 FROM pokemon_forms)))" % pokemon_form_ident),
         pokemon_form_ident,
         form_ident and ("'%s'" % form_ident) or 'NULL',
         "(SELECT id FROM pokemon WHERE identifier = '%s')" % (pokemon_ident if species['id'] in PSEUDOFORMS else pokemon_form_ident),
-        19 if pokemon_ident in ('meltan', 'melmetal') else 20,
-        "true" if pokemon['id'] < first_form_id else "false"
+        19 if pokemon_ident in ('meltan', 'melmetal') else 20
     ))
 
-    form_name = ("E'%s %s'" % (' '.join(word.capitalize() for word in form_ident.split('-')), species['name'])).encode('unicode-escape') if form_ident else 'NULL'
+    if pokemon['id'] > 893:
+        form_name = ("E'%s %s'" % (' '.join((word.capitalize() if word != 'galar' else 'Galarian') for word in form_ident.split('-')), species['name'])).encode('unicode-escape') if form_ident else 'NULL'
 
-    print("INSERT INTO pokemon_form_names (pokemon_form_id, local_language_id, form_name, pokemon_name) VALUES (%s, 9, %s, %s) ON CONFLICT DO NOTHING;" % (
-        pokemon['id'] if pokemon['id'] < first_form_id else ("(SELECT id FROM pokemon_forms WHERE identifier = '%s')" % pokemon_form_ident),
-        form_name,
-        form_name
-    ))
+        print("INSERT INTO pokemon_form_names (pokemon_form_id, local_language_id, form_name, pokemon_name) VALUES (%s, 9, %s, %s) ON CONFLICT DO NOTHING;" % (
+            pokemon['id'] if pokemon['id'] < first_form_id else ("(SELECT id FROM pokemon_forms WHERE identifier = '%s')" % pokemon_form_ident),
+            form_name,
+            form_name
+        ))
 
 print("COMMIT;")
